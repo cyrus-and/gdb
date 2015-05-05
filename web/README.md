3a7ce94516c1c9d1882f671dfd4696ba82312562
Andrea Cardaci <cyrus.and@gmail.com>
2015-05-05 23:47:35 +0200

    Fix web documentation and README consistency

--- web/README.md
+++ web/README.md
@@ -9,15 +9,15 @@ than one client.
 Several URLs are available:
 
 - a WebSocket will be bound to `NotificationsUrl`, every notification sent by
-  GDB will be delivered through this WebSocket as a JSON object.
+  GDB will be delivered through this WebSocket as a JSON object;
 
 - a WebSocket will be bound to `TerminalUrl`: data sent through it will be
   delivered to the target program's input whereas data read from it will reflect
-  the the target program's output.
+  the the target program's output;
 
 - POST requests are expected to `CommandsUrl` to send commands to GDB. Commands
   are represented by JSON arrays `[command, arguments...]`. Command responses
-  are sent back as JSON objects in the response body.
+  are sent back as JSON objects in the response body;
 
 - POST requests are expected to `InterruptUrl` to send interrupts to GDB, no
   parameters are allowed.
--- web/doc.go
+++ web/doc.go
@@ -3,19 +3,21 @@
 // Every new handler spawns a new GDB instance which is not meant to serve more
 // than one client.
 //
-// A WebSocket will be bound to NotificationsUrl, every notification sent by GDB
-// will be delivered through this WebSocket as a JSON object.
+// Several URLs are available:
 //
-// A WebSocket will be bound to TerminalUrl: data sent through it will be
-// delivered to the target program's input whereas data read from it will
-// reflect the the target program's output.
+// - a WebSocket will be bound to NotificationsUrl, every notification sent by
+//   GDB will be delivered through this WebSocket as a JSON object;
 //
-// POST requests are expected to CommandsUrl to send commands to GDB. Commands
-// are represented by JSON arrays [command, arguments...]. Command responses are
-// sent back as JSON objects in the response body.
+// - a WebSocket will be bound to TerminalUrl: data sent through it will be
+//   delivered to the target program's input whereas data read from it will
+//   reflect the the target program's output;
 //
-// POST requests are expected to InterruptUrl to send interrupts to GDB, no
-// parameters are allowed.
+// - POST requests are expected to CommandsUrl to send commands to GDB. Commands
+//   are represented by JSON arrays [command, arguments...]. Command responses
+//   are sent back as JSON objects in the response body;
+//
+// - POST requests are expected to InterruptUrl to send interrupts to GDB, no
+//   parameters are allowed.
 //
 // HTTP methods return 200 on success and 500 on error.
 package web
